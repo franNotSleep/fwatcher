a887c1c4de836234fa0be78a16b3f3295debda51
Francisco Jose lopez <frano04930@gmail.com>
2025-06-07 18:52:30 -0400

    some changes

--- main.go
+++ main.go
@@ -15,28 +15,73 @@ var (
 	cmdStr    = flag.String("c", "", "The command to execute when change detected")
 )
 
-func hasChange(filenames []string, lastModTime *time.Time) bool {
+type FWatchFS struct {
+	Files       map[string]*os.File
+	lastModTime time.Time
+}
+
+type FWModInfo struct {
+	HasChanged bool
+	Info       *os.FileInfo
+	File       *os.File
+}
+
+func NewFWatchFS() *FWatchFS {
+	return &FWatchFS{Files: make(map[string]*os.File), lastModTime: time.Now()}
+}
+
+func (fw *FWatchFS) HasChanged(filename string) *FWModInfo {
+	var fwModInfo FWModInfo
+
+	f, ok := fw.Files[filename]
+	if !ok {
+		of, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		f = of
+		fw.Files[filename] = of
+	}
+
+	info, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	fwModInfo.Info = &info
+	fwModInfo.File = f
+
+	if filename == "main.go" {
+		fmt.Printf("info mod: %s\n", info.ModTime())
+	} else {
+		fmt.Printf("filename %s\n", filename)
+	}
+
+	if info.ModTime().UnixMilli() > fw.lastModTime.UnixMilli() {
+		fw.lastModTime = info.ModTime()
+		fwModInfo.HasChanged = true
+		return &fwModInfo
+	}
+
+	fwModInfo.HasChanged = false
+	return &fwModInfo
+}
+
+func hasChange(filenames []string, fw *FWatchFS) bool {
 	for _, filename := range filenames {
 		if filename == "" {
 			continue
 		}
-		file, err := os.Open(filename)
-		if err != nil {
-			panic(err)
-		}
 
-		info, err := file.Stat()
-		if err != nil {
-			panic(err)
-		}
-
-		if info.ModTime().UnixMilli() > lastModTime.UnixMilli() {
-			*lastModTime = info.ModTime()
+		changeInfo := fw.HasChanged(filename)
+		if changeInfo.HasChanged {
 			return true
 		}
 
-		if info.IsDir() {
-			dirnames, err := file.ReadDir(0)
+		if (*changeInfo.Info).IsDir() {
+			dirnames, err := (*changeInfo.File).ReadDir(0)
+			fmt.Printf("%+v\n", dirnames)
 
 			if err != nil {
 				log.Fatal(err)
@@ -44,10 +89,11 @@ func hasChange(filenames []string, lastModTime *time.Time) bool {
 
 			names := make([]string, len(dirnames))
 			for _, dirname := range dirnames {
+				fmt.Printf("name: %s\n", dirname.Name())
 				names = append(names, fmt.Sprintf("%s/%s", filename, dirname.Name()))
 			}
 
-			if hasChange(names, lastModTime) {
+			if hasChange(names, fw) {
 				return true
 			}
 		}
@@ -67,10 +113,10 @@ func main() {
 	cmdName, cmdArgs := cmdSplit[0], cmdSplit[1:]
 
 	watchFilenames := strings.Split(*filenames, ",")
-	lastModTime := time.Now()
 
+	fw := NewFWatchFS()
 	for {
-		if hasChange(watchFilenames, &lastModTime) {
+		if hasChange(watchFilenames, fw) {
 			command := exec.Command(cmdName, cmdArgs...)
 			command.Stdout = os.Stdout
 
